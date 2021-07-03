14601f47bd76848a128efc34af7188c3ce3aa314
RikZun <rik.zunqq@gmail.com>
2021-07-03 23:14:36 +0300

    little update

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,24 @@
     <span class="param">technicalSkills</span>: [
       <span class="string">'JavaScript'</span>, <span class="string">'TypeScript'</span>,
       <span class="string">'HTML'</span>, <span class="string">'CSS'</span>, <span class="string">'SCSS'</span>,
-      <span class="string">'Vue.js'</span>, 
-      <span class="string">'Node.js'</span>, 
+      <span class="string">'React'</span>, <span class="string">'Vue.js'</span>, 
+      <span class="string">'Webpack'</span>, <span class="string">'Node.js'</span>, 
       <span class="string">'REST API'</span>
     ],
 
     <span class="param">familiarWith</span>: [
       <span class="string">'Nuxt.js'</span>,
       <span class="string">'Python'</span>,
+      <span class="string">'C++'</span>,
       <span class="string">'C#'</span>
     ],
 
-    <span class="param">workExperience</span>: <span class="reserved">null</span>,
+    <span class="param">workExperience</span>: [
+      <span class="param">sevenWinds</span>: {
+        <span class="param">position</span>: <span class="string">'Frontend React Developer'</span>,
+        <span class="param">time</span>: <span class="string">'{{Math.ceil((Date.now() - 16230132e5) / (1000 * 3600 * 24))}} days'</span>
+      }
+    ]
 
     <span class="param">acceptable</span>: {
       <span class="param">kindОfWork</span>: [
@@ -28,14 +34,20 @@
         <span class="string">'Частичная занятость'</span>,
         <span class="string">'Полная занятость'</span>
       ],
-      <span class="param">workSchedule</span>: [ <span class="string">'Удаленная работа'</span> ]
+      <span class="param">workSchedule</span>: [
+        <span class="string">'Полный день'</span>,
+        <span class="string">'Сменный график'</span>,
+        <span class="string">'Гибкий график'</span>,
+        <span class="string">'Удаленная работа'</span>
+      ]
     },
 
     <span class="param">desiredPositions</span>: [
+      <span class="string">'Frontend Developer'</span>,
       <span class="string">'Frontend Vue Developer'</span>,
-      <span class="string">'Backend TypeScript Developer'</span>,
+      <span class="string">'Frontend React Developer'</span>,
       <span class="string">'Backend JavaScript Developer'</span>,
-      <span class="string">'Frontend Developer'</span>
+      <span class="string">'Backend TypeScript Developer'</span>
     ],
 
     <span class="param">mail</span>: <span class="string">'<a href="mailto:rik.zunqq@gmail.com">rik.zunqq@gmail.com</a>'</span>,
@@ -61,22 +73,18 @@ export default class App extends Vue {}
 
 body
   margin: 0
-  color: white
-
-body
-  display: flex
-  align-items: center
-  justify-content: center
-  min-height: 100vh
-  background-color: #1E1E1E
   cursor: text
+  color: white
+  background-color: #1E1E1E
   font-family: Consolas, "Courier New", monospace
 
 pre
+  width: fit-content
   white-space: pre-wrap
-  margin: 0
-  margin: 10px
   line-height: 19px
+  left: 0
+  right: 0
+  margin-inline: auto
 
   > .reserved
     color: #569cd6
